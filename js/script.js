6c3d43e8213803b2bf60a3902696296aa9e07190
happysoni01 <166524499+happysoni01@users.noreply.github.com>
2025-02-03 16:36:47 +0530

    script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder=folder
-  let a = await fetch(`http://127.0.0.1:5500/${folder}/`);
+  let a = await fetch(`/${folder}/`);
   let response = await a.text();
   console.log(response);
 
@@ -86,7 +86,7 @@ const playMusic = (track, pause = false) => {
 };
 
 async function displayAlbums(){
-let a = await fetch(`http://127.0.0.1:5500/songs/`)
+let a = await fetch(`/songs/`)
 let response = await a.text();
 let div = document.createElement("div")
 div.innerHTML=response;
@@ -103,7 +103,7 @@ let array = Array.from(anchors)
     if (folder === "songs") {continue;} // Avoid duplicate 'songs/songs' issue
 
     // get the metadata of theh folder
-    let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`)
+    let a = await fetch(`/songs/${folder}/info.json`)
     let response = await a.json();
     console.log(response)
 
